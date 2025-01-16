a725371bb5e79277c4039d2e2644abea3c1e8d66
Edu Santos <edukadudu18@gmail.com>
2025-01-16 12:33:49 -0300

    alteracao para modules, uso de imports

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,75 +1,75 @@
-/// <reference path="base-component.ts"/>
-/// <reference path="../decorators/autobind.ts"/>
-/// <reference path="../state/project-state.ts"/>
-/// <reference path="../models/project.ts"/>
-/// <reference path="../models/drag-drop.ts"/>
+import { Component } from './base-component.js';
+import { DragTarget } from '../models/drag-drop.js';
+import { Project, ProjectStatus } from '../models/project.js';
+import { autobind } from '../decorators/autobind.js';
+import { projectState } from '../state/project-state.js';
+import { ProjectItem } from './project-item.js';
 
-namespace App {
-     // ProjectList
-    // responsavel pela visualizaçao na tela, e por pegar, editar as listas arrastaveis dos projetos, ativos e finalizados
-    export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
-        assignedProjects: Project[]; // referente ao listener, guardar os projetos
 
-        constructor(private type: 'active' | 'finished') {
-            super('project-list', 'app', false, `${type}-projects`); // super() para chamar o construtor da classe herdada
-            this.assignedProjects = [];
+// ProjectList
+// responsavel pela visualizaçao na tela, e por pegar, editar as listas arrastaveis dos projetos, ativos e finalizados
+export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
+    assignedProjects: Project[]; // referente ao listener, guardar os projetos
 
-            //  this.element.id = `${this.type}-projects`; // como iremos ter outra lista de projetos, cada um terá seu tipo especifico
-            this.configure();
-            this.renderContent();
-        }
+    constructor(private type: 'active' | 'finished') {
+        super('project-list', 'app', false, `${type}-projects`); // super() para chamar o construtor da classe herdada
+        this.assignedProjects = [];
 
-        @autobind
-        dragOverHandler(event: DragEvent) { // ativa ao entrar na area que pode ser removida
-            if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
-                event.preventDefault(); // ja que o default do js é nao permitir o event drag and drop
-                const listEl = this.element.querySelector('ul')!;
-                listEl.classList.add('droppable')
-            }
-        }
-        @autobind
-        dropHandler(event: DragEvent) {
-            const prjId = event.dataTransfer!.getData('text/plain');
-            projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished)
-        }
+        //  this.element.id = `${this.type}-projects`; // como iremos ter outra lista de projetos, cada um terá seu tipo especifico
+        this.configure();
+        this.renderContent();
+    }
 
-        @autobind
-        dragLeaveHandler(_: DragEvent) {
+    @autobind
+    dragOverHandler(event: DragEvent) { // ativa ao entrar na area que pode ser removida
+        if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault(); // ja que o default do js é nao permitir o event drag and drop
             const listEl = this.element.querySelector('ul')!;
-            listEl.classList.remove('droppable')
+            listEl.classList.add('droppable')
         }
+    }
+    @autobind
+    dropHandler(event: DragEvent) {
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished)
+    }
 
-        configure() {
-            this.element.addEventListener('dragover', this.dragOverHandler);
-            this.element.addEventListener('dragleave', this.dragLeaveHandler);
-            this.element.addEventListener('drop', this.dropHandler);
-            projectState.addListener((projects: Project[]) => {
-                const relevantProjects = projects.filter(prj => { // filtrar entre ativo e finalizado
-                    if (this.type === 'active') {
-                        return prj.status === ProjectStatus.Active;
-                    }
-                    return prj.status === ProjectStatus.Finished;
-                });
+    @autobind
+    dragLeaveHandler(_: DragEvent) {
+        const listEl = this.element.querySelector('ul')!;
+        listEl.classList.remove('droppable')
+    }
 
-                this.assignedProjects = relevantProjects; //estamos adicionando/sobreeescrevendo os  
-                this.renderProjects();            // assignedprojects com um novo projeto, posteriormente a ideia  
-                // é renderizar todos esses projetos
+    configure() {
+        this.element.addEventListener('dragover', this.dragOverHandler);
+        this.element.addEventListener('dragleave', this.dragLeaveHandler);
+        this.element.addEventListener('drop', this.dropHandler);
+        projectState.addListener((projects: Project[]) => {
+            const relevantProjects = projects.filter(prj => { // filtrar entre ativo e finalizado
+                if (this.type === 'active') {
+                    return prj.status === ProjectStatus.Active;
+                }
+                return prj.status === ProjectStatus.Finished;
             });
-        }
 
-        renderContent() {
-            const listId = `${this.type}-projects-list`;
-            this.element.querySelector('ul')!.id = listId;
-            this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+            this.assignedProjects = relevantProjects; //estamos adicionando/sobreeescrevendo os  
+            this.renderProjects();            // assignedprojects com um novo projeto, posteriormente a ideia  
+            // é renderizar todos esses projetos
+        });
+    }
 
-        }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
 
-        private renderProjects() {
-            const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
-            listEl.innerHTML = ''; // limpar todos os itens e depois renderiza novamente, evita a duplicaçao
-            for (const prjItem of this.assignedProjects) {
-                new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
-            }
+    }
+
+    private renderProjects() {
+        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        listEl.innerHTML = ''; // limpar todos os itens e depois renderiza novamente, evita a duplicaçao
+        for (const prjItem of this.assignedProjects) {
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         }
     }
 }
--- src/state/project-state.ts
+++ src/state/project-state.ts
@@ -1,55 +1,55 @@
-namespace App {
-     // Project State Management
-    // -> pensando em uma aplicaçao maior em que temo situações com mais states como, userState quando ele esta logado ou nao, um para projects, um para um shopping cart.
-    // -> pensando assim, algumas coisas iriam se repetir, como o array de listeners e o addlistener()
-    type Listener<T> = (items: T[]) => void; //criando type para classe listeners que antes estava como any[]
+import { Project, ProjectStatus } from "../models/project.js";
 
-    class State<T> {
-        protected listeners: Listener<T>[] = [];
+// Project State Management
+// -> pensando em uma aplicaçao maior em que temo situações com mais states como, userState quando ele esta logado ou nao, um para projects, um para um shopping cart.
+// -> pensando assim, algumas coisas iriam se repetir, como o array de listeners e o addlistener()
+type Listener<T> = (items: T[]) => void; //criando type para classe listeners que antes estava como any[]
 
-        addListener(listenerFn: Listener<T>) { // chamado quando algo muda no codigo, objetivo é renderizar nas listas os objetos adicionados (projetos no caso)
-            this.listeners.push(listenerFn);
-        }
+class State<T> {
+    protected listeners: Listener<T>[] = [];
+
+    addListener(listenerFn: Listener<T>) { // chamado quando algo muda no codigo, objetivo é renderizar nas listas os objetos adicionados (projetos no caso)
+        this.listeners.push(listenerFn);
+    }
+}
+
+// Project State Management
+export class ProjectState extends State<Project> {
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+        super();
     }
 
-    // Project State Management
-    export class ProjectState extends State<Project> {
-        private projects: Project[] = [];
-        private static instance: ProjectState;
-
-        private constructor() {
-            super();
-        }
-
-        static getInstance() {
-            if (this.instance) {
-                return this.instance;
-            }
-            this.instance = new ProjectState();
+    static getInstance() {
+        if (this.instance) {
             return this.instance;
         }
+        this.instance = new ProjectState();
+        return this.instance;
+    }
 
-        addProject(title: string, description: string, numOfPeople: number) {
-            const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);  // criado no Project, sempre um novo projeto estará como ativo por padrao
-            this.projects.push(newProject);
+    addProject(title: string, description: string, numOfPeople: number) {
+        const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);  // criado no Project, sempre um novo projeto estará como ativo por padrao
+        this.projects.push(newProject);
+        this.updateListeners();
+    }
+
+    // responsavel por pegar o id que será jogado na area arrastavel, e mover o objeto para essa area
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if (project && project.status !== newStatus) {
+            project.status = newStatus;
             this.updateListeners();
-        }
-
-        // responsavel por pegar o id que será jogado na area arrastavel, e mover o objeto para essa area
-        moveProject(projectId: string, newStatus: ProjectStatus) {
-            const project = this.projects.find(prj => prj.id === projectId);
-            if (project && project.status !== newStatus) {
-                project.status = newStatus;
-                this.updateListeners();
-            }
-        }
-
-        private updateListeners() {
-            for (const listenerFn of this.listeners) {
-                listenerFn(this.projects.slice()); // passa em uma copia do array, afim de evitar conflitos e bugs com o array original
-            }
         }
     }
 
-    export const projectState = ProjectState.getInstance(); // criando uma unica instancia globalmente do projeto
+    private updateListeners() {
+        for (const listenerFn of this.listeners) {
+            listenerFn(this.projects.slice()); // passa em uma copia do array, afim de evitar conflitos e bugs com o array original
+        }
+    }
 }
+
+export const projectState = ProjectState.getInstance(); // criando uma unica instancia globalmente do projeto
